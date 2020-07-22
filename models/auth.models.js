7c738568f5c23c9ba69f574d612555e442b0fde8
basitsattar <basitsattar850@gmail.com>
2020-07-22 22:43:02 +0500

    user field update

--- models/auth.models.js
+++ models/auth.models.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
-  username: {
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
     type: String,
     required: true,
   },
--- schema/auth.schema.js
+++ schema/auth.schema.js
@@ -1,7 +1,8 @@
 const Joi = require("@hapi/joi");
 
 const signupSchema = Joi.object().keys({
-  username: Joi.string().alphanum().min(3).max(30).required(),
+  firstName: Joi.string().alphanum().min(3).max(30).required(),
+  lastName: Joi.string().alphanum().min(3).max(30).required(),
   email: Joi.string().min(5).max(255).required().email(),
   password: Joi.string().min(5).max(30).required(),
   // companyName: Joi.string().min(3).max(50).required(),
